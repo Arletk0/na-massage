3e2b785f806bf40eac0a933a7dc2332cb6d9921c
Arletk0 <193724438+Arletk0@users.noreply.github.com>
2025-06-16 16:23:31 +0300

    feat: enhance website content and layout with new sections, images, and interactive features

--- src/main.js
+++ src/main.js
@@ -1,2 +1,15 @@
 import '../styles/modern-normalize.css';
 import '../styles/style.css';
+
+
+const scrollContainer = document.querySelector('.prices-scroll');
+const btnLeft = document.getElementById('scroll-left');
+const btnRight = document.getElementById('scroll-right');
+
+btnLeft.addEventListener('click', () => {
+    scrollContainer.scrollBy({left: -300, behavior: 'smooth'});
+});
+
+btnRight.addEventListener('click', () => {
+    scrollContainer.scrollBy({left: 300, behavior: 'smooth'});
+});
